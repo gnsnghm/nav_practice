df2674acad629767636996ae01bcbe6690096513
gnsnghm <41932520+gnsnghm@users.noreply.github.com>
2023-09-30 05:47:13 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LOGO from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <img
+          src={LOGO}
+          width="30"
+          height="30"
+          alt="React Bootstrap logo"
+        />
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>a
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar >
+    );
+  }
 }
-
 export default App;
