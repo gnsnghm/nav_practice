4dd70bd588a95be546410208a4d1b096af77fee1
gnsnghm <41932520+gnsnghm@users.noreply.github.com>
2023-10-01 14:44:14 +0000

    add auto accordion

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import LOGO from './logo.svg';
 class App extends Component {
   render() {
     return (
-      <Navbar bg="dark" variant="dark">
-        <img
-          src={LOGO}
-          width="30"
-          height="30"
-          alt="React Bootstrap logo"
-        />
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>a
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Navbar >
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src={LOGO}
+            width="30"
+            height="30"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>a
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
